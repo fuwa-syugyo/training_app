e8937742300490db7d31df5d8233a1abed9ef58c
fuwa <fuwafuwa41.glr@gmail.com>
2020-06-02 00:33:49 +0000

    main page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+ 
+  root  'main_page#home' 
+  get 'main_page/home'
+  get 'main_page/help'
+  get 'main_page/about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
