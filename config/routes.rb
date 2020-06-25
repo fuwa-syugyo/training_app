faddc1480ba1666025724e77c55bcfcb4f32a9c4
Lucypacapaca <fuwafuwa41.glr@gmail.com>
2020-06-25 23:45:34 +0000

    ユーザー登録フォーム作成

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get '/about', to: 'main_page#about'
   get '/contact', to: 'main_page#contact'
   get '/signup', to: 'users#new'
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
