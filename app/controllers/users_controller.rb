df145881c0846686e1958adc61223c05c7e7ebf4
Lucypacapaca <fuwafuwa41.glr@gmail.com>
2020-06-27 12:53:42 +0000

    ユーザー登録ページ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,21 @@ class UsersController < ApplicationController
     @user = User.new
   end
   
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "ようこそ筋トレアプリへ！"
+      redirect_to @user
+    else
+      render 'new'
+      
+    end
+  end
+  
+  private
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+  
 end
