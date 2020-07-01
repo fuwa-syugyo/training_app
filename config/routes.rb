04e773e928407a4290b40dbb89c7de4ebdc789e8
Lucypacapaca <fuwafuwa41.glr@gmail.com>
2020-07-01 00:36:22 +0000

    ログアウト処理実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
  
+  get 'sessions/new'
+
   get 'users/new'
 
   root  'main_page#home' 
@@ -8,6 +10,10 @@ Rails.application.routes.draw do
   get '/about', to: 'main_page#about'
   get '/contact', to: 'main_page#contact'
   get '/signup', to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  
   resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
