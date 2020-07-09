1851aff8b9d6890f3446cd5ab000c311198cec73
Lucypacapaca <fuwafuwa41.glr@gmail.com>
2020-07-09 13:59:35 +0000

    ユーザー情報ページの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200705022908) do
+ActiveRecord::Schema.define(version: 20200709130805) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20200705022908) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
