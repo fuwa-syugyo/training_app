52c0424708a8490b660dc13639dfa1e1c534ddf2
Lucypacapaca <fuwafuwa41.glr@gmail.com>
2020-07-13 23:08:22 +0000

    メール設定追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,8 +98,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
+    :user_name      => ENV['app175958851@heroku.com'],
+    :password       => ENV['uefdmkvr5805'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
