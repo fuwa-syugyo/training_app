a5b0a7cb8c036e8af33e8ee2f6b1773dc45ce6b0
Lucypacapaca <fuwafuwa41.glr@gmail.com>
2020-07-30 00:03:31 +0000

    パスワードの再設定（東京）

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,6 +1,8 @@
 class PasswordResetsController < ApplicationController
   before_action :get_user, only:[:edit, :update]
   before_action :valid_user, only:[:edit, :update]
+  before_action :check_expration, only: [:edit, :update]
+  
   def new
   end
   
@@ -17,15 +19,32 @@ class PasswordResetsController < ApplicationController
     end
   end
   
-  
-  
 
   def edit
     
   end
   
+  def update
+    if params[:user][:password].empty?
+      @user.errors.add(:password, :blank)
+      render 'edit'
+    elsif @user.update_attributes(user_params)
+      log_in @user
+      @user.update_attribute(:reset_digest, nil)
+      flash[:success] = "パスワードが再設定されました"
+      redirect_to @user
+    else  
+        render 'edit'
+    end
+  end
+  
+  
   private
   
+    def user_params
+      params.require(:user).permit(:password, :password_confirmation)
+    end
+    
     def get_user
       @user = User.find_by(email: params[:email]) 
     end
@@ -35,6 +54,13 @@ class PasswordResetsController < ApplicationController
         redirect_to root_url
       end
         
+    end
+    
+    def check_expration
+      if @user.password_reset_expired?
+        flash[:danger] = "パスワードの有効期限が切れています"
+        redirect_to new_password_reset_url
+      end
     end
   
   
